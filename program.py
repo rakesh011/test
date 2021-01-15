30f2e94ee707469cd3eb5487d38b7145ffbb6bef
Rakesh Iyer <rakesh.iyer011@gmail.com>
2021-01-15 13:02:07 +0530

    This is a new addition to the code

--- program.py
+++ program.py
@@ -1,3 +1,5 @@
 num = 8
 num_sqrt = num ** 0.5
 print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
+num_sqrt = cmath.sqrt(num)
+print('The square root of {0} is {1:0.3f}+{2:0.3f}j'.format(num ,num_sqrt.real,num_sqrt.imag))
